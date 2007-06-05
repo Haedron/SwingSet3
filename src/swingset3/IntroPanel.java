9c0e0fc4ca08b022a33d5849f49a83ba3bc1eebd
aim <aim@4398f88d-df26-0410-a7eb-fe6337abf351>
2007-06-05 21:25:18 +0000

    Added bsd copyright notice.
    git-svn-id: https://swingset3.dev.java.net/svn/swingset3/trunk@59 4398f88d-df26-0410-a7eb-fe6337abf351

--- src/swingset3/IntroPanel.java
+++ src/swingset3/IntroPanel.java
@@ -1,10 +1,32 @@
 /*
- * IntroPanel.java
+ * Copyright %YEARS% Sun Microsystems, Inc.  All Rights Reserved.
  *
- * Created on May 23, 2007, 10:34 PM
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
  *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Sun Microsystems nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package swingset3;
@@ -114,14 +136,14 @@ public class IntroPanel extends JComponent {
             PropertySetter gleamer = new PropertySetter(gleam, "x", GLEAM_X, GLEAM_X_MAX);
             gleamAnimator = new Animator(500, gleamer);
             gleamAnimator.setStartDelay(20);
-            gleamAnimator.setAcceleration(.2f);
-            gleamAnimator.setDeceleration(.3f);  
+            gleamAnimator.setAcceleration(.3f);
+            gleamAnimator.setDeceleration(.1f);  
             
             PropertySetter sparkler = new PropertySetter(sparkle, "alpha", 0.0f, 1.0f);
             Animator sparkleAnimator = new Animator(600, sparkler);
             sparkleAnimator.setStartDelay(20);
             sparkleAnimator.setAcceleration(.2f);
-            sparkleAnimator.setDeceleration(.3f);  
+            sparkleAnimator.setDeceleration(.1f);  
        
             PropertySetter glower = new PropertySetter(glowingTitle, "alpha", 0.0f, 1.0f);
             glowAnimator = new Animator(1200, glower);
--- src/swingset3/SwingSet3.java
+++ src/swingset3/SwingSet3.java
@@ -1,11 +1,35 @@
-
-/* SwingSet3.java
+/*
+ * Copyright %YEARS% Sun Microsystems, Inc.  All Rights Reserved.
  *
- * Created on June 2, 2006, 1:57 PM
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *   - Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   - Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *
+ *   - Neither the name of Sun Microsystems nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
 package swingset3;
-
 
 import java.awt.BorderLayout;
 import java.awt.Color;
@@ -84,19 +108,9 @@ public class SwingSet3 extends JFrame  {
             "swingset3.demos.toplevels.JWindowDemo",
                     
             "swingset3.demos.controls.JButtonDemo",
-            "swingset3.demos.controls.JCheckBoxesDemo",
-            "swingset3.demos.controls.JComboBoxesDemo",
-            "swingset3.demos.controls.JLabelsDemo",
             
-            "swingset3.demos.data.JListsDemo",
-            "swingset3.demos.data.JTablesDemo",
-            "swingset3.demos.data.JTreesDemo",
-            
-            "swingset3.demos.text.JEditorPanesDemo",
+            "swingset3.demos.data.JTablesDemo"
                                         
-            "swingset3.layoutContainers.JPanelDemo",
-            "swingset3.layoutContainers.JTabbedPaneDemo",
-            "swingset3.layoutContainers.JScrollPaneDemo"
     };
         
     static {
