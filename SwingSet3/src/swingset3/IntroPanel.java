a08edabc6f4bc98d9d9696ccaaee846d75106a9a
aim <aim@4398f88d-df26-0410-a7eb-fe6337abf351>
2007-11-10 01:52:23 +0000

    Cleaned up and simplified animation
    git-svn-id: https://swingset3.dev.java.net/svn/swingset3/trunk@80 4398f88d-df26-0410-a7eb-fe6337abf351

--- SwingSet3/src/swingset3/IntroPanel.java
+++ SwingSet3/src/swingset3/IntroPanel.java
@@ -34,19 +34,23 @@ package swingset3;
 import java.awt.AlphaComposite;
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Composite;
 import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.util.Map;
 import javax.imageio.ImageIO;
-import javax.swing.JComponent;
-import javax.swing.JTextArea;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
 import org.jdesktop.animation.timing.Animator;
 import org.jdesktop.animation.timing.interpolation.PropertySetter;
 import org.jdesktop.animation.timing.triggers.TimingTrigger;
@@ -57,7 +61,7 @@ import org.jdesktop.animation.timing.triggers.TimingTriggerEvent;
  * @author aim
  */
 //remind(aim): this class needs to be converted to use Components instead of Node
-public class IntroPanel extends JComponent {
+public class IntroPanel extends JPanel {
     private static final int GLEAM_X = 55;
     private static final int GLEAM_Y = 122;
     private static final int GLEAM_X_MAX = 362;
@@ -71,20 +75,21 @@ public class IntroPanel extends JComponent {
     protected Node glowingTitle;
     protected Node gleam;
     protected Node sparkle;
-    protected Node message;
     
     protected Animator gleamAnimator;
+    protected Animator sparkleAnimator;
     protected Animator glowAnimator;
-
-    protected JTextArea messageText;
-    protected float messageAlpha;
+    protected Animator glowFadeAnimator;
+    protected Animator slideAnimator;
+    
+    private boolean animating = false;
     
     public IntroPanel(int preferredWidth, int preferredHeight) {
         super();
-        setLayout(new BorderLayout());
+        setLayout(null);
 
-        initialize();
         setPreferredSize(new Dimension(preferredWidth, preferredHeight));
+        initialize();
         
         // Temporary hook to re-run animation
         addMouseListener(new MouseAdapter() {
@@ -95,107 +100,100 @@ public class IntroPanel extends JComponent {
         });
     }
     
-    protected void initialize() {
+    protected void initialize() {        
         try {
-            title = new ImageNode("resources/images/splash_graphics.png", 0, 0);
+            title = new ImageNode("resources/images/splash_title_duke.png", 0, 0);
             glowingTitle = new ImageNode("resources/images/swingset3_glow.png", 0, 0, 0.0f);
             gleam = new ImageNode("resources/images/gleam.png", GLEAM_X, GLEAM_Y, 0.0f);
             sparkle = new ImageNode("resources/images/sparkle2.png", HELMET_X, HELMET_Y, 0.0f);
-            message = new ImageNode("resources/images/message2.png", 0, 0, 0.0f);
              
-        } catch (IOException e) {
-            
+        } catch (IOException e) {            
         }
         gradientColors = new Color[2];
-        gradientColors[0] = new Color(208, 207, 202);
+        //gradientColors[0] = new Color(208, 207, 202);
+        gradientColors[0] = UIManager.getColor("Panel.background");
         gradientColors[1] = new Color(32, 32, 37); 
         
-        gleamAnimator = null;
+        PropertySetter gleamer = new PropertySetter(gleam, "x", GLEAM_X, GLEAM_X_MAX);
+        gleamAnimator = new Animator(500, gleamer);
+        gleamAnimator.setStartDelay(20);
+        gleamAnimator.setAcceleration(.9f);
+        gleamAnimator.setDeceleration(.1f);
         
+        PropertySetter sparkler = new PropertySetter(sparkle, "alpha", 0.0f, 1.0f);
+        sparkleAnimator = new Animator(600, sparkler);
+        sparkleAnimator.setStartDelay(20);
+        sparkleAnimator.setAcceleration(.2f);
+        sparkleAnimator.setDeceleration(.1f);
         
+        PropertySetter glower = new PropertySetter(glowingTitle, "alpha", 0.0f, 1.0f);
+        glowAnimator = new Animator(1200, glower);
+        glowAnimator.setAcceleration(.4f);
+        glowAnimator.setDeceleration(.3f);
+        glowAnimator.setStartDelay(700);
+        
+        TimingTrigger.addTrigger(gleamAnimator, glowAnimator, TimingTriggerEvent.STOP);
+        
+        PropertySetter glowFader = new PropertySetter(glowingTitle, "alpha", 1.0f, 0.6f);
+        glowFadeAnimator = new Animator(600, glowFader);
+        glowFadeAnimator.setAcceleration(.3f);
+        glowFadeAnimator.setDeceleration(.1f);
+        glowFadeAnimator.setStartDelay(300);
+        
+        TimingTrigger.addTrigger(glowAnimator, glowFadeAnimator, TimingTriggerEvent.STOP);
+        
+        animating = false;
     }
     
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
         Rectangle bounds = getBounds();
         
         if (backgroundImage == null ||
                 backgroundImage.getWidth(this) != bounds.width ||
                 backgroundImage.getHeight(this) != bounds.height) {
-            
+            // cache gradient
             backgroundImage = Utilities.createGradientImage(bounds.width, bounds.height,
-                    gradientColors[0], gradientColors[1]);
-            
+                    gradientColors[0], gradientColors[1]);            
         }
         int x = 0;
         int y = 0;
         
         g.drawImage(backgroundImage, x, y, null);
         
-        if (gleamAnimator == null) {
-            PropertySetter gleamer = new PropertySetter(gleam, "x", GLEAM_X, GLEAM_X_MAX);
-            gleamAnimator = new Animator(500, gleamer);
-            gleamAnimator.setStartDelay(20);
-            gleamAnimator.setAcceleration(.9f);
-            gleamAnimator.setDeceleration(.1f);  
-            
-            PropertySetter sparkler = new PropertySetter(sparkle, "alpha", 0.0f, 1.0f);
-            Animator sparkleAnimator = new Animator(600, sparkler);
-            sparkleAnimator.setStartDelay(20);
-            sparkleAnimator.setAcceleration(.2f);
-            sparkleAnimator.setDeceleration(.1f);  
-       
-            PropertySetter glower = new PropertySetter(glowingTitle, "alpha", 0.0f, 1.0f);
-            glowAnimator = new Animator(1200, glower);
-            glowAnimator.setAcceleration(.4f);
-            glowAnimator.setDeceleration(.3f);
-            glowAnimator.setStartDelay(700);           
-            
-            TimingTrigger.addTrigger(gleamAnimator, glowAnimator, TimingTriggerEvent.STOP);
-            
-            //initFadeAnimator(duke);
-            initFadeAnimator(title);
-            initFadeAnimator(glowingTitle);
-            initFadeAnimator(gleam);
-            initFadeAnimator(sparkle);
-            
-            PropertySetter messageFadeIn = new PropertySetter(message, "alpha", 0.0f, 1.0f);
-            Animator messageAnimator = new Animator(1000, messageFadeIn);
-            messageAnimator.setStartDelay(100);
-            messageAnimator.setAcceleration(.3f);
-            messageAnimator.setDeceleration(.1f);
-            TimingTrigger.addTrigger(glowAnimator, messageAnimator, TimingTriggerEvent.STOP);
-      
+        if (!animating) {
             gleam.setAlpha(1.0f);
             gleamAnimator.start();
             sparkleAnimator.start();
-
+            animating = true;
         }
+        
         // adjust position of painting to center the graphics
         int dx = Math.max(0, (bounds.width - title.getWidth())/2);
         int dy = Math.max(0, (bounds.height - title.getHeight())/2);
         g.translate(dx, dy);
         
-        glowingTitle.paint(g);
-        title.paint(g);
-        gleam.paint(g);
-        sparkle.paint(g);        
-        message.paint(g);
+        glowingTitle.paint((Graphics2D)g);
+        title.paint((Graphics2D)g);
+        gleam.paint((Graphics2D)g);
+        sparkle.paint((Graphics2D)g);        
   
     }
     
     protected void initFadeAnimator(Node node) {
-        PropertySetter fader = new PropertySetter(node, "alpha", node.getAlpha(), 0.0f);
-        Animator fadeAnimator = new Animator(1000, fader);
+        PropertySetter fader = new PropertySetter(node, "alpha", 1.0f, 0.6f);
+        Animator fadeAnimator = new Animator(5000, fader);
         fadeAnimator.setAcceleration(.3f);
         fadeAnimator.setDeceleration(.1f);
         fadeAnimator.setStartDelay(300);
         TimingTrigger.addTrigger(glowAnimator, fadeAnimator, TimingTriggerEvent.STOP);
     }
     
-    public void setMessageAlpha(float alpha) {
-        this.messageAlpha = alpha;
-        repaint();
+    protected void initSlideAnimator(Node node, int startx) {
+        PropertySetter slider = new PropertySetter(node, "x", startx, startx + getWidth());
+        slideAnimator = new Animator(1000, slider);
+        slideAnimator.setAcceleration(.5f);
+        slideAnimator.setStartDelay(800);
+        TimingTrigger.addTrigger(glowFadeAnimator, slideAnimator, TimingTriggerEvent.STOP);
     }
     
     // remind(aim): could just use java.awt.Component. ?
@@ -242,7 +240,7 @@ public class IntroPanel extends JComponent {
             int oldWidth = this.width;
             this.width = width;
             if (width != oldWidth) {
-                repaint();
+                repaint(x, y, Math.max(oldWidth,width), height);
             }
         }
         
@@ -254,7 +252,7 @@ public class IntroPanel extends JComponent {
             int oldHeight = this.height;
             this.height = height;
             if (height != oldHeight) {
-                repaint();
+                repaint(x, y, width, Math.max(oldHeight, height));
             }
         }
         
@@ -262,11 +260,15 @@ public class IntroPanel extends JComponent {
             return height;
         }
         
+        public Rectangle getBounds() {
+            return new Rectangle(x, y, width, height);
+        }
+        
         public void setAlpha(float alpha) {
             float oldAlpha = this.alpha;
             this.alpha = alpha;
             if (alpha != oldAlpha) {
-                repaint();
+                repaint(x, y, width, height);
             }
         }
         
@@ -274,7 +276,7 @@ public class IntroPanel extends JComponent {
             return alpha;
         }  
         
-        public abstract void paint(Graphics g);
+        public abstract void paint(Graphics2D g);
     }
         
     public class ImageNode extends Node {
@@ -292,9 +294,9 @@ public class IntroPanel extends JComponent {
             this.height = image.getHeight();
         }
         
-        public void paint(Graphics g) {
-            if (alpha > 0) {
-                Graphics2D g2 = (Graphics2D)g;            
+        public void paint(Graphics2D g) {
+            if (alpha > 0 && g.getClipBounds().intersects(getBounds())) {
+                Graphics2D g2 = g;
                 if (alpha < 1.0) {
                     g2 = (Graphics2D)g.create();
                     g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, alpha));
@@ -302,5 +304,5 @@ public class IntroPanel extends JComponent {
                 g2.drawImage(image, getX(), getY(), getWidth(), getHeight(), null);
             }
         }                
-    }
+    }    
 }
