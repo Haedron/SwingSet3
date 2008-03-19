24b590028a2c15a007a150f5a0ed8875b73c3470
aim <aim@4398f88d-df26-0410-a7eb-fe6337abf351>
2008-03-19 20:54:15 +0000

    Upgraded tooltip demo with new toolbox image.
    git-svn-id: https://swingset3.dev.java.net/svn/swingset3/trunk@141 4398f88d-df26-0410-a7eb-fe6337abf351

--- SwingSet3/src/swingset3/demos/general/ToolTipDemo.java
+++ SwingSet3/src/swingset3/demos/general/ToolTipDemo.java
@@ -31,12 +31,10 @@
 
 package swingset3.demos.general;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Point;
-import java.awt.Polygon;
-import javax.swing.Box;
-import javax.swing.BoxLayout;
+import java.awt.Rectangle;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import swingset3.DemoProperties;
@@ -71,95 +69,86 @@ public class ToolTipDemo extends DemoBase {
      * ToolTipDemo Constructor
      */
     public ToolTipDemo() {
-	// Set the title for this demo, and an icon used to represent this
-	// demo inside the SwingSet2 app.
 	super();
 
-	// Set the layout manager.
 	JPanel p = getDemoPanel();
-	p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
-	p.setBackground(Color.white);
+        p.setLayout(new BorderLayout());
 
-	// Create a Cow to put in the center of the panel.
-	Cow cow = new Cow();
-	cow.getAccessibleContext().setAccessibleName(getString("ToolTipDemo.accessible_cow"));
-
-	// Set the tooltip text. Note, for fun, we also set more tooltip text
-	// descriptions for the cow down below in the Cow.contains() method.
-	cow.setToolTipText(getString("ToolTipDemo.cow"));
-
-	// Add the cow midway down the panel
-	p.add(Box.createRigidArea(new Dimension(1, 150)));
-	p.add(cow);
-    }
-
-
-    class Cow extends JLabel {
-	Polygon cowgon = new Polygon();
-	
-	public Cow() {
-	    super(createImageIcon("tooltip/cow.gif", getString("ToolTipDemo.bessie")));
-	    setAlignmentX(CENTER_ALIGNMENT);
-
-	    // Set polygon points that define the outline of the cow.
-	    cowgon.addPoint(3,20);    cowgon.addPoint(44,4);
-	    cowgon.addPoint(79,15);   cowgon.addPoint(130,11);
-	    cowgon.addPoint(252,5);   cowgon.addPoint(181,17);
-	    cowgon.addPoint(301,45);  cowgon.addPoint(292,214);
-	    cowgon.addPoint(269,209); cowgon.addPoint(266,142);
-	    cowgon.addPoint(250,161); cowgon.addPoint(235,218);
-	    cowgon.addPoint(203,206); cowgon.addPoint(215,137);
-	    cowgon.addPoint(195,142); cowgon.addPoint(143,132);
-	    cowgon.addPoint(133,189); cowgon.addPoint(160,200);
-	    cowgon.addPoint(97,196);  cowgon.addPoint(107,182);
-	    cowgon.addPoint(118,185); cowgon.addPoint(110,144);
-	    cowgon.addPoint(59,77);   cowgon.addPoint(30,82);
-	    cowgon.addPoint(30,35);   cowgon.addPoint(15,36);
-	}
-	
-	boolean moo = false;
-	boolean milk = false;
-	boolean tail = false;
-
-	// Use the contains method to set the tooltip text depending
-	// on where the mouse is over the cow.
-	public boolean contains(int x, int y) {
-	    if(!cowgon.contains(new Point(x, y))) {
-		return false;
-	    }
-	    
-	    if((x > 30) && (x < 60) && (y > 60) && (y < 85)) {
-		if(!moo) {
-		    setToolTipText("<html><center><font color=blue size=+2>" +
-				   getString("ToolTipDemo.moo") + "</font></center></html>");
-		    moo = true;
-		    milk = false;
-		    tail = false;
-		}
-	    } else if((x > 150) && (x < 260) && (y > 90) && (y < 145)) {
-		if(!milk) {
-		    setToolTipText("<html><center><font face=AvantGarde size=+1 color=white>" +
-				   getString("ToolTipDemo.got_milk") + "</font></center></html>");
-		    milk = true;
-		    moo = false;
-		    tail = false;
-		}
-	    } else if((x > 280) && (x < 300) && (y > 20) && (y < 175)) {
-		if(!tail) {
-		    setToolTipText("<html><em><b>" + getString("ToolTipDemo.tail") + "</b></em></html>");
-		    tail = true;
-		    moo = false;
-		    milk = false;
-		}
-	    } else if(moo || milk || tail) {
-		setToolTipText(getString("ToolTipDemo.tooltip_features"));
-		moo = false;
-		tail = false;
-		milk = false;
-	    }
-
-	    return true;
-	}
+	// Create a panel which contains specific tooltip regions.
+        Toolbox toolbox = new Toolbox();
+                
+	p.add(toolbox, BorderLayout.CENTER);
     }
     
+    public class Toolbox extends JPanel {
+        private Rectangle plainRect = new Rectangle(44, 0, 186, 128);
+        private Rectangle htmlRect = new Rectangle(240, 134, 186, 186);
+        private Rectangle styledRect = new Rectangle(45, 327, 188, 134);
+        
+        private JLabel background;
+        private JComponent plainToolTipRegion;
+        private JComponent htmlToolTipRegion;
+        private JComponent styledToolTipRegion;
+        
+        public Toolbox() {
+            super();
+            setLayout(null);
+            
+            background = new JLabel(createImageIcon("tooltip/tooltip_background.png",
+                                                     getString("ToolTipDemo.toolbox")));
+            
+            background.setVerticalAlignment(JLabel.TOP);
+            background.setHorizontalAlignment(JLabel.LEFT);
+            
+            // Note: tooltip text isn't retrieved from properties file in order
+            // to make this code easier to understand
+            
+            
+            //<snip>Create region for displaying plain tooltip
+            plainToolTipRegion = createToolTipRegion(getString("ToolTipDemo.plain"));
+            plainToolTipRegion.setToolTipText("A simple one line tip.");
+            //</snip>
+
+            //<snip>Create region for displaying HTML tooltip
+            htmlToolTipRegion = createToolTipRegion(getString("ToolTipDemo.html"));
+            htmlToolTipRegion.setToolTipText("<html><body bgcolor=\"#AABBFF\">In case you thought that tooltips had to be<p>"+
+                    "boring, one line descriptions, the <font color=blue size=+2>Swing!</font> team<p>"+
+                    "is happy to shatter your illusions.<p>"+
+                    "In Swing, you can use HTML to <ul><li>Have Lists<li><b>Bold</b> text<li><em>emphasized</em>"+
+                    "text<li>text with <font color=red>Color</font><li>text in different <font size=+3>sizes</font>"+
+                    "<li>and <font face=AvantGarde>Fonts</font></ul>Oh, and they can be multi-line, too.</body></html>");
+            //</snip>
+            
+            //<snip>Create region for displaying styled tooltip            
+            styledToolTipRegion = createToolTipRegion(getString("ToolTipDemo.styled"));
+            styledToolTipRegion.setToolTipText("<html>Tips can be styled to be"+ 
+                    "<br><b>interesting</b> and <i>fun</i></html>");
+            //</snip>
+ 
+            add(htmlToolTipRegion);
+            add(styledToolTipRegion);
+            add(plainToolTipRegion);
+ 
+            add(background);
+
+;        }
+
+        public void doLayout() {
+            background.setBounds(0, 0, getWidth(), getHeight());
+            plainToolTipRegion.setBounds(plainRect);
+            htmlToolTipRegion.setBounds(htmlRect);
+            styledToolTipRegion.setBounds(styledRect);       
+        }  
+        
+        protected JComponent createToolTipRegion(String text) {
+            JLabel region = new JLabel(text);
+
+            region.setForeground(Color.white);
+            region.setFont(getFont().deriveFont(18f));
+            region.setHorizontalAlignment(JLabel.CENTER);
+            region.setVerticalAlignment(JLabel.CENTER);
+            
+            return region;
+        }
+    }
 }
